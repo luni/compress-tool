8696fb585005d2b9b0934d33d141f50d74443330
lunix <lunix@mirkx.org>
2026-01-04 02:10:21 +0100

    Update hash-folder parameter handling.

--- hash-folder.sh
+++ hash-folder.sh
@@ -3,11 +3,11 @@ if [ -z "$1" ]; then
     echo "Usage: $0 <directory>"
     exit 1
 fi
-cd "$1"
+
 if [ -z "$2" ]; then
-    HASH_FILENAME="$(basename "$(pwd)").sha256"
+    HASH_FILENAME="$(basename "$1").sha256"
 else
     HASH_FILENAME="$2.sha256"
 fi
 
-hashdeep -l -c sha256 -r . | awk -F, '!/^#/ {print $2 "  " $3}' | tee "$HASH_FILENAME"
+hashdeep -l -c sha256 -r "$1" | awk -F, '!/^#/ {print $2 "  " $3}' | tee "$HASH_FILENAME"
